e95c15796e7f871991866e2c4a56fd0236d89060
Pyrox <poojanrathod@gmail.com>
2018-02-23 10:57:42 +0530

    Update StartScreen.java

--- src/screens/StartScreen.java
+++ src/screens/StartScreen.java
@@ -1,44 +1,141 @@
 package screens;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Label;
-import java.awt.Toolkit;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.*;
 
-public class StartScreen {
+public class StartScreen extends Frame implements MouseListener{
+	
+	JLabel game_title,player1,player2,name1,name2;
+    JTextField p1_name,p2_name;
+    JButton p1_ready,p2_ready,start;
+    Boolean isp1 = false,isp2 = false;
+    
+    
 	  StartScreen(){
-		    JFrame frame = new JFrame();
-		    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		    frame.setSize(screenSize.width,screenSize.height);
+		  
+		  prepareGUI();
 		    
-		    
-		    Label game_title,player1,player2,name1,name2;
-		    
-		    game_title = new Label("Quizzo...");
-			
-			
-			game_title.setBounds(screenSize.width/2 - 150 , 100 , 300, 200);
-		    game_title.setFont(new Font("DpQuake", Font.BOLD,75));
-		    frame.add(game_title);
-		   
-		    player1 = new Label("Player 1");
-			
-			
-			player1.setBounds((screenSize.width*1)/8, (screenSize.height *2)/5, 80, 40);
-		    frame.add(player1);
-		   
-		    
-		    
-		    
-		   
-		    frame.setLayout(null);
-		    frame.setVisible(true);
 		  }
+	  
 
 		  public static void main(String args[]){
 		    StartScreen f = new StartScreen();
 		  }
+		  
+		  
+		  private void checkstart() {
+			  if(isp1 && isp2) {
+				  start.setVisible(true);
+			  }
+		  }
+		  
+		  private void prepareGUI() {
+
+			 
+			    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			    this.setSize(screenSize.width,screenSize.height);
+			     
+			    
+			    
+			    
+			    game_title = new JLabel("Quizzo...");
+			    game_title.setBounds(screenSize.width/2 - 150 , 100 , 300, 200);
+			    game_title.setFont(new Font("DpQuake", Font.BOLD,75));
+			    this.add(game_title);
+			   
+			    player1 = new JLabel("Player 1");
+			    player1.setBounds((screenSize.width*1)/8, (screenSize.height *2)/5, 80, 40);
+			    this.add(player1);
+			   
+			    player2 = new JLabel("Player 2");
+			    player2.setBounds((screenSize.width*7)/8 - 80, (screenSize.height *2)/5, 80, 40);
+			    this.add(player2);
+			    
+			    p1_name = new JTextField("");
+			    p1_name.setBounds((screenSize.width*1)/8 - 40, (screenSize.height *1)/2, 200, 40);
+			    this.add(p1_name);
+			    
+			    p2_name = new JTextField("");
+			    p2_name.setBounds((screenSize.width*7)/8 - 100, (screenSize.height *1)/2, 200, 40);
+			    this.add(p2_name);
+			    
+			    p1_ready = new JButton("Ready");
+			    p1_ready.setBounds((screenSize.width*1)/8 - 40, (screenSize.height *3)/5, 100, 40);
+			    p1_ready.addMouseListener(this);
+			    this.add(p1_ready);
+			    
+			    
+			    p2_ready = new JButton("Ready");
+			    p2_ready.setBounds((screenSize.width*7)/8 - 100, (screenSize.height *3)/5, 100, 40);
+			    p2_ready.addMouseListener(this);
+			    this.add(p2_ready);
+			    
+			    start = new JButton("Start");
+			    start.setBounds((screenSize.width*1)/2 - 100, (screenSize.height *4)/5, 100, 40);
+			    start.setVisible(false);
+			    start.addMouseListener(this);
+			    this.add(start);
+			   
+			   
+			    
+			   
+			    
+			    
+			    
+			   
+			    this.setLayout(null);
+			    this.setVisible(true);
+			   
+		  }
+
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			
+			if(e.getComponent().equals(p1_ready)){
+				isp1 = true;
+				checkstart();
+			}
+			if(e.getComponent().equals(p2_ready)){
+				isp2 = true;
+				checkstart();
+			}
+			if(e.getComponent().equals(start)) {
+				System.out.println("Hello");
+				this.dispose();
+				QuizScreen q = new QuizScreen();
+			}
+		
+		}
+
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 }
